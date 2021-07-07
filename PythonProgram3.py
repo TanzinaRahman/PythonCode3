8ddb9396f7571378ee33176777b7215aae8300b9
Tanzina Rahman Mona <77913975+TanzinaRahman@users.noreply.github.com>
2021-07-08 00:10:26 +0600

    Create PythonProgram3.py

---
+++ PythonProgram3.py
@@ -0,0 +1,10 @@
+# use variable
+# name , age , cgpa etc
+name = "Tanzina Rahman"
+age = 22
+cgpa = 3.90
+print("My name is" + name)
+print("She is currently" , age , "years old")
+print(name + "Lives in Dhaka")
+print("At the age of" , age , ", She has started to learn Python")
+print("She scored " , cgpa , "in her exam")
